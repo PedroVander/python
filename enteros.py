0b304ffd67715af95f9a671830023cd8466b6fd1
unknown <2btc23@iesramonllull.local>
2023-01-27 09:05:34 +0100

    raiz cuadrada

--- enteros.py
+++ enteros.py
@@ -3,11 +3,16 @@ import math
 a = int(input("num escribir mucho: "))
 b = int(input("num escribir mucho: "))
 c = int(input("num escribir mucho: "))
-print(a+b)
-print(a-b)
-print(a*b)
-print(a**b)
-print(a/b)
+disc = int(((b**2)-(4*a*c)))
+ 
+if disc < 0:
+    print("Ta mal no sale")
+  
 
-print(-b*+sqrt(b**2-(4*a+c)) /2*a)
-print(-b*-sqrt(b**2-(4*a+c)) /2*a)
+if disc == 0:
+    print("tiene 1 soluçao")
+if disc > 0:
+    print("tiene 2 soluçao")
+    
+print((-b+math.sqrt(b**2-(4*a*c))/(2*a)))
+print((-b-math.sqrt(b**2-(4*a*c))/(2*a)))
