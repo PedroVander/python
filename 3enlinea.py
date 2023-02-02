2eda1bb6874b36da8ceb61f141126fed77431e92
unknown <2btc23@IESRAMONLLULL.LOCAL>
2023-02-02 13:06:33 +0100

    tres en raya acabado

--- 3enlinea.py
+++ 3enlinea.py
@@ -3,7 +3,7 @@ import random
 
 
 casilla=[" "," "," "," "," "," "," "," "," "]
-
+op=[0,1,2,3,4,5,6,7,8]
 '''
 |x| | |  casilla 0, 1, 2
 |0| | |  casilla 3, 4, 5
@@ -17,53 +17,108 @@ while jugar:
     print("|"+casilla[3] + "|" +casilla[4]+ "|" +casilla[5]+"|")
     print("|"+casilla[6] + "|" +casilla[7]+ "|" +casilla[8]+"|")
     
-    jugabalidad=0 
+    jugo=0
+    
     posicion= int(input("Elige casilla del 0 al 8 "))
     casilla[posicion]="X"
-
-    if 
-    jugabilidad=+1
+    if posicion in op:
+        casilla[posicion]="X"
+        op.remove(posicion)
+    else:
+        print("Ya esta ocupado")
+        posicion= int(input("Elige casilla del 0 al 8 "))
+        casilla[posicion]="X"
+        op.remove(pos)
 
     if casilla[0] == casilla[1] == casilla[2] == "X":
         print("Ganador!")
         jugar=False
-        
-    if casilla[0] == casilla[1] == casilla[2] == "O":
-        print("Perdedor!")
-        jugar=False
-
+        break
+ 
     if casilla[3] == casilla[4] == casilla[5] == "X":
         print("Ganador!")
         jugar=False
-    if casilla[3] == casilla[4] == casilla[5] == "O":
-        print("Perdedor!")
-        jugar=False
+        break
+
 
     if casilla[6] == casilla[7] == casilla[8] == "X":
         print("Ganador!")
         jugar=False
-    if casilla[6] == casilla[7] == casilla[8] == "O":
-        print("Perdedor!")
-        jugar=False
+        break
+    
 
     if casilla[0] == casilla[4] == casilla[8] == "X":
         print("Ganador!")
         jugar=False
-    if casilla[0] == casilla[4] == casilla[8] == "O":
-        print("Perdedor!")
-        jugar=False
+        break
 
     if casilla[2] == casilla[4] == casilla[6] == "X":
         print("Ganador!")
         jugar=False
+        break
+  
+        
+    if casilla[1] == casilla[4] == casilla[7] == "X":
+        print("Ganador!")
+        jugar=False
+        break
+
+    if casilla[0] == casilla[3] == casilla[6] == "X":
+        print("Ganador!")
+        jugar=False
+        break
+  
+
+    if casilla[2] == casilla[5] == casilla[8] == "X":
+        print("Ganador!")
+        jugar=False
+        break
+    
+
+    if jugo==9:
+        jugar=False
+        print("Empate!")
+        break
+        
+    jugo=jugo+1
+    
+
+    ordenador=random.choice(op)
+    casilla[ordenador]="O"
+    op.remove(ordenador)
+    if casilla[2] == casilla[5] == casilla[8] == "O":
+        print("Perdedor!")
+        jugar=False
+        break
+    if casilla[0] == casilla[3] == casilla[6] == "O":
+        print("Perdedor!")
+        jugar=False
+        break
+    if casilla[1] == casilla[4] == casilla[7] == "O":
+        print("Perdedor!")
+        jugar=False
+        break
     if casilla[2] == casilla[4] == casilla[6] == "O":
         print("Perdedor!")
         jugar=False
-    if jugabilidad=9
-        print("Empate!")
+        break
+    if casilla[0] == casilla[4] == casilla[8] == "O":
+        print("Perdedor!")
         jugar=False
-    
-   
+        break
+    if casilla[6] == casilla[7] == casilla[8] == "O":
+        print("Perdedor!")
+        jugar=False
+        break
+    if casilla[3] == casilla[4] == casilla[5] == "O":
+        print("Perdedor!")
+        jugar=False
+        break
+    if casilla[0] == casilla[1] == casilla[2] == "O":
+        print("Perdedor!")
+        jugar=False
+        break
+
 
 print("|"+casilla[0] + "|" +casilla[1]+ "|" +casilla[2]+"|")
 print("|"+casilla[3] + "|" +casilla[4]+ "|" +casilla[5]+"|")
