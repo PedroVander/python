4e7e77f1ef1fe135d1abb64686356439d103bd60
unknown <2btc23@iesramonllull.local>
2023-02-16 12:44:08 +0100

    Blackjack casi acabado

--- Blackjack/juego.py
+++ Blackjack/juego.py
@@ -6,7 +6,13 @@ mibaraja = Baraja()
 mano1 = Mano()
 
 mibaraja.mezclar()
-mibaraja.mostrar()
+# mibaraja.mostrar()
 cartacogida = mibaraja.coger_carta()
 mano1.añadir_carta(cartacogida)
+cartacogida = mibaraja.coger_carta()
+mano1.añadir_carta(cartacogida)
+
 mano1.mostrar_mano()
+mano1.calcula_valor()
+
+print(mano1.valor)
--- Blackjack/mano.py
+++ Blackjack/mano.py
@@ -1,3 +1,5 @@
+
+
 class Mano:
     def __init__(self):
         self.cartas = []
@@ -9,3 +11,13 @@ class Mano:
     def mostrar_mano(self):
         for carta in self.cartas:
             print(carta)
+    def calcula_valor(self): 
+        self.valor=0
+        for carta in self.cartas:
+            if carta.valor in ["Reina", "Rey", "Jota", "10" ]:
+                self.valor +=10
+            elif carta.valor == "As":
+                self.valor +=11
+            else:
+                self.valor += int(carta.valor) # 1 <-"1"
+
