cba80c3ab7f514460c0b8b0d8290b8db52e759e5
unknown <2btc23@IESRAMONLLULL.LOCAL>
2023-02-16 13:06:32 +0100

    Acabado blacjack

--- Blackjack/juego.py
+++ Blackjack/juego.py
@@ -11,8 +11,30 @@ cartacogida = mibaraja.coger_carta()
 mano1.añadir_carta(cartacogida)
 cartacogida = mibaraja.coger_carta()
 mano1.añadir_carta(cartacogida)
-
 mano1.mostrar_mano()
 mano1.calcula_valor()
-
 print(mano1.valor)
+
+while True:
+    estado=mano1.valor
+    if estado ==21:
+        print("BlackoJacko, has ganado")
+        break
+    if estado > 21:
+        print("Has perdido que malo")
+        break
+    pedir_carta = input("Quieres otra carta ? s/n ")
+    if pedir_carta == "s":
+        cartacogida = mibaraja.coger_carta()
+        mano1.añadir_carta(cartacogida)
+        mano1.mostrar_mano()
+        mano1.calcula_valor()
+        print(mano1.valor)
+    else:
+        break
+        print("Gracias por jugar")
+    
+
+    
+   
+
